2d7ef5a93a061e96a67287056efc2b6ccc8dd88a
Mixail <mixail.any@gmail.com>
2021-01-23 21:24:45 +0300

    up return

--- example_01_2/hello_bin/src/main.cpp
+++ example_01_2/hello_bin/src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <greeting.hpp>
 
-int main(int /*argc*/, char */*argv*/[])
+int main(int /*argc*/, char * /*argv*/[])
 {
     const char *user_var = std::getenv("USER");
     const char *host_var = std::getenv("HOSTNAME");
@@ -9,7 +9,9 @@ int main(int /*argc*/, char */*argv*/[])
     std::string_view user = user_var != nullptr ? user_var : "USER - environment variable not found";
     std::string_view host = host_var != nullptr ? host_var : "HOST - environment variable not found";
 
-    bool is_good = greeting(user, host);
-    int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
-    return result;
+    // bool is_good = greeting(user, host);
+    // int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
+    // return result;
+
+    return !greeting(user, host);
 }
